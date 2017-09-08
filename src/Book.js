e43b95309c3c48eefc5c50bf74ebe08197fe9517
Thierry Michel <thierry.michel@hotmail.com>
2017-09-08 23:27:09 +1000

    feat: Populate shelves with React

---
+++ src/Book.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+
+class Book extends Component {
+	
+	static propTypes = {		
+		book: PropTypes.object.isRequired
+	}
+	
+	state = {
+	}
+
+
+	render() {		
+		const {book} = this.props
+		return (
+	      <div className="book">
+	        <div className="book-top">
+	          <div className="book-cover" 
+	          			style={{ 
+	          				width: 128, 
+	          				height: 193, 
+	          				backgroundImage: `url(${book.imageLinks.thumbnail})`}}/>
+
+	        </div>
+	        <div className="book-title">{book.title}</div>
+	        <div className="book-authors">{book.authors[0]}</div>
+	      </div>	            
+	  )
+	}
+}
+
+
+export default Book
---
+++ src/Shelf.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+// import escapeRegExp from 'escape-string-regexp'
+// import sortBy from 'sort-by'
+// import { Link }  from 'react-router-dom'
+import Book from './Book'
+
+
+
+class Shelf extends Component {
+	static propTypes = {		
+		shelf: PropTypes.object.isRequired
+	}
+	
+	state = {
+		query: ''
+	}
+
+
+	render() {
+		const {shelf} = this.props
+		return (
+			<div className="bookshelf">
+				<h2 className="bookshelf-title">{shelf.title}</h2>
+	        <div className="bookshelf-books">
+	          <ol className="books-grid">
+	          	{this.props.shelf.books.map((book) => (
+		            <li key={book.id}>
+		              	<Book 
+		              		book={book}
+		              	/>
+		            </li>		          		
+	          	))}
+
+            </ol>
+          </div>
+        </div>	    
+	  )
+	}
+}
+
+
+export default Shelf
