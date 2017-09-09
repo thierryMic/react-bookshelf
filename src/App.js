81d90c023c478f8fe2bae7c9bed5ca0e80f3198d
Thierry Michel <thierry.michel@hotmail.com>
2017-09-09 14:39:27 +1000

    feat: Add routing to search page

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import React from 'react'
 import * as BooksAPI from './BooksAPI'
 import './App.css'
 import {Route} from 'react-router-dom'
+import { Link }  from 'react-router-dom'
 import Shelf from './Shelf'
+import Search from './Search'
 
 class BooksApp extends React.Component {
 
@@ -25,7 +27,7 @@ class BooksApp extends React.Component {
       this.setState((state) => ({
         shelves : this.state.shelves
       }))     
-  
+
   }
 
   componentDidMount() {
@@ -55,6 +57,10 @@ class BooksApp extends React.Component {
             </div>
           )}
         />
+
+      
+        <Link className='open-search open-search-link' to='/search'>Add a book</Link>
+        <Route path="/search" component={Search}/>
       </div>
     )
   }
