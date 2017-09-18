a192c2e767edf923ca1e12b3186e319a29358c6e
Thierry Michel <thierry.michel@hotmail.com>
2017-09-18 16:20:59 +1000

    refactor: refactored the Star component as a stateless functional component

--- src/Star.js
+++ src/Star.js
@@ -1,42 +1,31 @@
-import React, { Component } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 
 
 /**
 * @description Represents a star - usually used as part of a collection withihn a Rater component
-* @constructor
+* @description evaluates the active prop to determine the star character to be displayed
+* @description and the css classes to apply to the span element which encloses the star
+* @description calls relevant parent component functions when the user hover, clicks, etc
 */
-class Star extends Component {
-	static propTypes = {
+const Star = (props) => {
+	const {value, rate, onHover, onUnhover, active} = props
+	return (
+			<span
+				className={active ? 'star star-selected' : 'star'}
+				onClick={(e) => rate(value)}
+				onMouseEnter={(e) => onHover(value)}
+				onMouseLeave={(e) => onUnhover()}
+			>{active ? '★' : '☆'}</span>
+  )
+}
+
+Star.propTypes = {
 		value: PropTypes.number.isRequired,
 		active: PropTypes.bool.isRequired,
 		rate: PropTypes.func.isRequired,
 		onHover: PropTypes.func.isRequired,
 		onUnhover: PropTypes.func.isRequired,
-	}
-
-
-	/**
-	* @description renders a Star component
-	* @description evaluates the active prop to determine the star character to be displayed
-	* @description and the css classes to apply to the span element which encloses the star
-	* @description calls relevant parent component functions when the user hover, clicks, etc
-	*/
-	render() {
-		const {value, rate, onHover, onUnhover, active} = this.props
-		const classes = active ? 'star star-selected' : 'star'
-		const char = active ? '★' : '☆'
-
-		return (
-				<span
-					className={classes}
-					onClick={(e) => rate(value)}
-					onMouseEnter={(e) => onHover(value)}
-					onMouseLeave={(e) => onUnhover()}
-				>{char}</span>
-	  )
-	}
 }
-
 
 export default Star
